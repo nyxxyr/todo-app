17c320072b5b854367485252c67ab271dce72fe0
nyxxyr <nyxxyr@gmail.com>
2020-08-30 18:46:29 +0700

    finishing app

---
+++ src/components/Todo.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Todo = ({text, todo, todos, setTodos}) => {
+    // Events
+    const deleteHandler = () => {
+        setTodos(todos.filter((el) => el.id !== todo.id));
+    };
+    const completeHandler = () => {
+        setTodos(todos.map((el) => {
+            if(el.id === todo.id){
+                return {
+                    ...el, completed: !el.completed
+                }
+            }
+            return el;
+        }))
+    }
+    return(
+        <div className="todo">
+            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{todo.text}</li>
+            <button onClick={completeHandler} className="complete-btn">
+                <i className="fas fa-check"></i>
+            </button>
+            <button onClick={deleteHandler} className="trash-btn">
+                <i className="fas fa-trash"></i>
+            </button>
+        </div>
+    );
+}
+
+export default Todo;
